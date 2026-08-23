816f6b16f1ccc1daf0d706030750a086db26e820
fedinnaire2000-ui <fedinnaire2000@gmail.com>
2026-08-23 14:35:20 +0100

    src/main.jsx
    import React from "react";
    import ReactDOM from "react-dom/client";
    import TrueCVApp from "./TrueCVApp.jsx";
    import "./index.css";
    
    ReactDOM.createRoot(document.getElementById("root")).render(
      <React.StrictMode>
        <TrueCVApp />
      </React.StrictMode>
    );

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import TrueCVApp from "./TrueCVApp.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TrueCVApp />
+  </React.StrictMode>
+);
